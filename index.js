81e8972ff66500463ba9538a2ce352de9d4374aa
unknown <anamulislamshamim117@gmail.com>
2022-12-27 18:51:03 +0600

    created the counterReducer

--- index.js
+++ index.js
@@ -35,4 +35,21 @@ const addUser = (user) => {
 // 1. state
 // 2. dispatch action
 // 3. reducer
-// 4. 
+// 4. store
+// create reducer
+const counterReducer = (state=initialCounterState, action) => {
+    switch(action.type){
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1,
+            }
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
+        default:
+            //
+    }
+}
